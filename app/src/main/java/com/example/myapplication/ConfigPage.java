526061f4f3d1dbdf98ff767bd7d9892feebdf44e
Vinayak Athavale <vathavale6@gatech.edu>
2023-02-12 15:31:23 -0500

    Put some changes

--- app/src/main/java/com/example/myapplication/ConfigPage.java
+++ app/src/main/java/com/example/myapplication/ConfigPage.java
@@ -2,13 +2,30 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ConfigPage extends AppCompatActivity {
+
+    private Button game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config_page);
+    }
+
+
+    public void setUpConfigPage(Bundle savedInstanceState) {
+
+        game = (Button) findViewById(R.id.goToGame);
+        game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ConfigPage.this, Game.class);
+                startActivity(intent);            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/myapplication/Game.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Game extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.initial_game_screen);
+    }
+
+
+
+
+
+
+
+}
