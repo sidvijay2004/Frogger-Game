8e9d54c88c8151f4059dfa0598fc671c696b4ffc
Vinayak Athavale <vathavale6@gatech.edu>
2023-02-12 15:48:48 -0500

    Made some changes

--- app/src/main/java/com/example/myapplication/Game.java
+++ app/src/main/java/com/example/myapplication/Game.java
@@ -1,8 +1,11 @@
 package com.example.myapplication;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Game extends AppCompatActivity {
     @Override
