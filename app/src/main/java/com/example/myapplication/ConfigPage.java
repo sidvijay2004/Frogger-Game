5d85b9f81a37c26be5b2134e6ba1256805f3efb3
sbhat97 <skbhat@ucdavis.edu>
2023-02-13 17:22:57 -0500

    Converted Radio buttons to toggle options to improve UI.

--- app/src/main/java/com/example/myapplication/ConfigPage.java
+++ app/src/main/java/com/example/myapplication/ConfigPage.java
@@ -4,12 +4,15 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
-
+import android.widget.TextView;
 
 
 public class ConfigPage extends AppCompatActivity {
@@ -22,12 +25,16 @@ public class ConfigPage extends AppCompatActivity {
     private RadioGroup difficultyGroup;
     private RadioGroup characterGroup;
 
+    private ImageButton charSelectLeft;
+    private ImageButton charSelectRight;
+    private ImageView charImageDisplay;
+    private int charSelectionId;
 
-
+    private TextView difficultySelector;
+    private ImageButton diffSelectLeft;
+    private ImageButton diffSelectRight;
+    private int diffId;
     public static final String NAME_ID = "name1";
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,51 +43,143 @@ public class ConfigPage extends AppCompatActivity {
         setUpConfigPage(savedInstanceState);
     }
 
+    public void updateCharDisplay() {
+        switch(charSelectionId) {
+            case 0:
+                charImageDisplay.setImageResource(R.drawable.character_one);
+                break;
+            case 1:
+                charImageDisplay.setImageResource(R.drawable.character_two);
+                break;
+            default:
+                charImageDisplay.setImageResource(R.drawable.character_three);
+                break;
+        }
+    }
+
+    public void updateDiffId() {
+        diffId = diffId%3;
+        String diffText = "";
+        switch(diffId) {
+            case 0:
+                diffText = "EASY";
+                break;
+            case 1:
+                diffText = "MEDIUM";
+                break;
+            default:
+                diffText = "HARD";
+                break;
+        }
+        difficultySelector.setText(diffText);
+    }
+
+
     public void setUpConfigPage(Bundle savedInstanceState) {
 
         game = (Button) findViewById(R.id.goToGame);
 
         name = (EditText) findViewById(R.id.personName);
 
-        difficultyGroup = (RadioGroup) findViewById(R.id.ChooseDifficulty);
 
-        characterGroup = (RadioGroup) findViewById(R.id.ChooseCharacter);
+        charImageDisplay = (ImageView) findViewById(R.id.characterDisplay);
+        charSelectLeft = (ImageButton) findViewById(R.id.charLeft);
+        charSelectRight = (ImageButton) findViewById(R.id.charRight);
+
+        difficultySelector = (TextView) findViewById(R.id.difficultySelector);
+        diffSelectLeft = (ImageButton) findViewById(R.id.diffLeft);
+        diffSelectRight = (ImageButton) findViewById(R.id.diffRight);
+        diffId = 0;
+        charSelectionId = 0;
+        diffSelectLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                diffId--;
+                updateDiffId();
+            }
+        });
+
+        diffSelectRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                diffId++;
+                updateDiffId();
+            }
+        });
 
 
+        charSelectLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                charSelectionId--;
+                charSelectionId %= 3;
+                updateCharDisplay();
+            }
+        });
+
+        charSelectRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                charSelectionId++;
+                charSelectionId %= 3;
+                updateCharDisplay();
+            }
+        });
 
         game.setOnClickListener(new View.OnClickListener() {
-
-
             @Override
             public void onClick(View v) {
 
-                if (name.getText().toString().trim().equals("")
-                        || (difficultyGroup.getCheckedRadioButtonId() == -1)
-                        || (characterGroup.getCheckedRadioButtonId() == -1)) {
-
-                    //System.out.println("Null name");
+                if (name.getText().toString().trim().equals("")) {
                 } else {
+
+                    String diffString = difficultySelector.getText().toString();
+
                     Intent intent = new Intent(ConfigPage.this, GameScreen.class);
                     intent.putExtra(NAME_ID, name.getText().toString());
 
-                    if (characterGroup.getCheckedRadioButtonId() == R.id.CharacterOne) {
-                        intent.putExtra("image", R.drawable.character_one);
-                    } else if (characterGroup.getCheckedRadioButtonId() == R.id.CharacterTwo) {
-                        intent.putExtra("image", R.drawable.character_two);
-                    } else if (characterGroup.getCheckedRadioButtonId() == R.id.CharacterThree) {
-                        intent.putExtra("image", R.drawable.character_three);
+                    switch(charSelectionId) {
+                        case 0:
+                            intent.putExtra("image", R.drawable.character_one);
+                            break;
+                        case 1:
+                            intent.putExtra("image", R.drawable.character_two);
+                            break;
+                        default:
+                            intent.putExtra("image", R.drawable.character_three);
+                            break;
                     }
+//                    if (characterGroup.getCheckedRadioButtonId() == R.id.CharacterOne) {
+//                        intent.putExtra("image", R.drawable.character_one);
+//                    } else if (characterGroup.getCheckedRadioButtonId() == R.id.CharacterTwo) {
+//                        intent.putExtra("image", R.drawable.character_two);
+//                    } else if (characterGroup.getCheckedRadioButtonId() == R.id.CharacterThree) {
+//                        intent.putExtra("image", R.drawable.character_three);
+//                    }
 
-                    if (difficultyGroup.getCheckedRadioButtonId() == R.id.DifficultyOne) {
-                        intent.putExtra("difficulty", "Easy");
-                        intent.putExtra("numLives", 10);
-                    } else if (difficultyGroup.getCheckedRadioButtonId() == R.id.DifficultyTwo) {
-                        intent.putExtra("difficulty", "Intermediate");
-                        intent.putExtra("numLives", 7);
-                    } else if (difficultyGroup.getCheckedRadioButtonId() == R.id.DifficultyThree) {
-                        intent.putExtra("difficulty", "Difficult");
-                        intent.putExtra("numLives", 5);
+                    switch(diffString) {
+                        case "MEDIUM":
+                            intent.putExtra("difficulty", "Medium");
+                            intent.putExtra("numLives", 7);
+                            break;
+                        case "HARD":
+                            intent.putExtra("difficulty", "Hard");
+                            intent.putExtra("numLives", 5);
+                            break;
+                        default:
+                            intent.putExtra("difficulty", "Easy");
+                            intent.putExtra("numLives", 10);
                     }
+//                    if (difficultyGroup.getCheckedRadioButtonId() == R.id.DifficultyOne) {
+//                        intent.putExtra("difficulty", "Easy");
+//                        intent.putExtra("numLives", 10);
+//                    } else if (difficultyGroup.getCheckedRadioButtonId() == R.id.DifficultyTwo) {
+//                        intent.putExtra("difficulty", "Intermediate");
+//                        intent.putExtra("numLives", 7);
+//                    } else if (difficultyGroup.getCheckedRadioButtonId() == R.id.DifficultyThree) {
+//                        intent.putExtra("difficulty", "Difficult");
+//                        intent.putExtra("numLives", 5);
+//                    }
 
                     startActivity(intent);
 
