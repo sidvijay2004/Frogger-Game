bbe78c256436abb04cdfdcfaa8fc1d79eac4691c
Sid Vijay <svijayasankar3@gatech.edu>
2023-03-12 19:49:40 -0400

    Added vehicle obstacles with movement

--- app/src/main/java/com/example/myapplication/GameScreen.java
+++ app/src/main/java/com/example/myapplication/GameScreen.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ValueAnimator;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.KeyEvent;
@@ -15,6 +17,10 @@ public class GameScreen extends AppCompatActivity {
     private TextView gameScreenName;
     private ImageView character;
 
+    private ImageView vehicle1;
+    private ImageView vehicle2;
+    private ImageView vehicle3;
+
     private TextView difficultyAndNumLives;
 
     private int width;
@@ -25,6 +31,9 @@ public class GameScreen extends AppCompatActivity {
 
     private Player gameCharacter;
 
+    int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +43,12 @@ public class GameScreen extends AppCompatActivity {
         gameScreenName = (TextView) findViewById(R.id.nameGameScreen);
         character = (ImageView) findViewById(R.id.sprite);
         difficultyAndNumLives = (TextView) findViewById(R.id.difficultyAndNumLives);
+
+        vehicle1 = (ImageView) findViewById(R.id.vehicle1);
+        vehicle2 = (ImageView) findViewById(R.id.vehicle2);
+        vehicle3 = (ImageView) findViewById(R.id.vehicle3);
+
+
 
         String userName = getIntent().getStringExtra(ConfigPage.NAME_ID);
         //String difficultyLevel = getIntent().getStringExtra("difficulty");
@@ -59,6 +74,64 @@ public class GameScreen extends AppCompatActivity {
         int startTileHeight = startTile.getHeight();
         bottomHeightBounds = (startTileYPos + startTileHeight / 2 - 50);
         gameCharacter.setPosition((int) character.getX(), (int) character.getY());
+
+        moveVehicles();
+
+    }
+
+
+    private void moveVehicles() {
+        // Vehicle 1 moves from right to left
+        ValueAnimator animator1 = ValueAnimator.ofFloat(screenWidth, -vehicle1.getWidth());
+        animator1.setDuration(5000);
+        animator1.setRepeatMode(ValueAnimator.RESTART);
+        animator1.setRepeatCount(ValueAnimator.INFINITE);
+        animator1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float x = (float) animation.getAnimatedValue();
+                vehicle1.setX(x);
+                if (x < -vehicle1.getWidth()) {
+                    vehicle1.setX(screenWidth);
+                }
+            }
+        });
+
+        // Vehicle 3 moves from right to left
+        ValueAnimator animator3 = ValueAnimator.ofFloat(screenWidth, -vehicle3.getWidth());
+        animator3.setDuration(6500);
+        animator3.setRepeatMode(ValueAnimator.RESTART);
+        animator3.setRepeatCount(ValueAnimator.INFINITE);
+        animator3.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float x = (float) animation.getAnimatedValue();
+                vehicle3.setX(x);
+                if (x < -vehicle3.getRight()) {
+                    vehicle3.setX(screenWidth);
+                }
+            }
+        });
+
+// Vehicle 2 moves from left to right
+        ValueAnimator animator2 = ValueAnimator.ofFloat(-vehicle2.getWidth(), screenWidth);
+        animator2.setDuration(8000);
+        animator2.setRepeatMode(ValueAnimator.RESTART);
+        animator2.setRepeatCount(ValueAnimator.INFINITE);
+        animator2.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float x = (float) animation.getAnimatedValue();
+                vehicle2.setX(x);
+                if (x >= screenWidth) {
+                    vehicle2.setX(-vehicle2.getWidth());
+                }
+            }
+        });
+
+        animator1.start();
+        animator2.start();
+        animator3.start();
 
     }
 
