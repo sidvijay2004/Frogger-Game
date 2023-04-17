f29cb66826e5d9eec96b888a5591108433049095
Siddharth Vijayasankar <sidvijay2004@gmail.com>
2023-04-16 22:34:59 -0400

    Adding new test cases

--- app/src/test/java/com/example/myapplication/Sprint5Tests.java
+++ app/src/test/java/com/example/myapplication/Sprint5Tests.java
@@ -97,7 +97,42 @@ public class Sprint5Tests {
     }
 
 
+    @Test
+    public void testPlayerScoreUpdateVehicleCombo1() {
+        Player player = new Player();
+        Vehicle v1 = new Vehicle(  R.id.vehicle1, 0, "Medium");
+        Vehicle v2 = new Vehicle(  R.id.vehicle2, 60, "Big");
+        Vehicle v3 = new Vehicle(  R.id.vehicle3, 90, "Small");
+        player.setPosY(0);
+        player.setBoundsTop(-10000);
+        player.moveUp(v1, v2, v3);
+        player.moveUp(v1, v2, v3);
+        player.moveUp(v1, v2, v3);
+        player.moveUp(v1, v2, v3);
+        assertEquals(3505, player.getScore());
+    }
 
+    @Test
+    public void testPlayerScoreUpdateVehicleCombo2() {
+        Player player = new Player();
+        Vehicle v1 = new Vehicle(  R.id.vehicle1, 0, "Medium");
+        Vehicle v2 = new Vehicle(  R.id.vehicle2, 50, "Big");
+        Vehicle v3 = new Vehicle(  R.id.vehicle3, 100, "Small");
+        player.setPosY(0);
+        player.setBoundsTop(-10000);
+        player.moveUp(v1, v2, v3); // 50
+        assertEquals(player.getScore(), 500);
+        player.moveUp(v1, v2, v3); // 100
+        assertEquals(player.getScore(), 1500);
+
+        player.moveDown();
+
+        player.moveDown();
+        player.moveUp(v1, v2, v3);
+        player.moveUp(v1, v2, v3);
+        assertEquals(player.getScore(), 1500);
+
+    }
 
 
 }
